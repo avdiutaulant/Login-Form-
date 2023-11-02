1b5f4d384f20cf5fe780a5440e5a0213332f839e
Ollioo <taulantavdiu@gmail.com>
2023-11-02 14:38:25 +0100

    "Today I have made login with spring security with mssql with none , bcrypt algorithm and using custom tables with bcrypt algorithm you can find this query that i post here"

---
+++ 01-spring-boot-spring-mvc-security-default/spring-mvc-security-jdbc/sql-scripts/05-setup-spring-security-demo-database-bcrypt.sql
@@ -0,0 +1,56 @@
+USE `employee_directory`;
+
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: https://www.bcryptcalculator.com/encode
+--
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `users` 
+VALUES 
+('sara','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('erda','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('taulanti','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities4_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('sara','ROLE_EMPLOYEE'),
+('erda','ROLE_EMPLOYEE'),
+('erda','ROLE_MANAGER'),
+('taulanti','ROLE_EMPLOYEE'),
+('taulanti','ROLE_MANAGER'),
+('taulanti','ROLE_ADMIN');
--- 01-spring-boot-spring-mvc-security-default/src/main/java/com/security/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/security/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -6,13 +6,47 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
 import java.net.http.HttpRequest;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    
+    // this is for custom tables that we create in database that spring need to know to read this custom tables
+    // I configured spring security to use our custom database tables
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=? "
+        );
+
+        // define query to retrieve the authorities/roles by username
+                jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                        "select user_id, role from roles where user_id=?"
+                );
+
+        return jdbcUserDetailsManager;
+    }
+    
+    
+    // this is for none and bcrypt algorithm with tables that spring security have their tables
+//    @Bean
+//    public UserDetailsManager userDetailsManager(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+
+/*
+
+// this is hard coded without databases
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
 
@@ -36,6 +70,8 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(sara,erda,taulanti);
     }
+*/
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
